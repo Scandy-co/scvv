027c708d4bffb3153a4744e49bc3b1cae2bb8fcc
H. Cole Wiley <cole@scandy.co>
2020-02-14 17:27:56 -0600

    remove unused code

--- javascript/lib/workers/LoadSCVVWorker.js
+++ javascript/lib/workers/LoadSCVVWorker.js
@@ -71,21 +71,6 @@ const nativeAttributeMap = {
   uv: "TEX_COORD"
 }
 
-var BASE64_MARKER = ";base64,"
-
-function convertDataURIToBinary(dataURI) {
-  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length
-  var base64 = dataURI.substring(base64Index)
-  var raw = atob(base64)
-  var rawLength = raw.length
-  var array = new Uint8Array(new ArrayBuffer(rawLength))
-
-  for (var i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i)
-  }
-  return array
-}
-
 /**
  * Get the Draco Decoder module from
  */
