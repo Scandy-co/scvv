3c7cfd00afe16bf14c0bc0ce3629ff891a340d2a
H. Cole Wiley <cole@scandy.co>
2020-03-04 19:46:33 -0600

    add new transform for version 0.2.0

--- javascript/utils.js
+++ javascript/utils.js
@@ -64,6 +64,7 @@ const downloadAudioBuffer = (audioCtx, url) =>
 module.exports.downloadAudioBuffer = downloadAudioBuffer
 
 const getSCVVTransform = version => {
+  console.log('getSCVVTransform version', version)
   if (!version) {
     return [
       -0.0,
@@ -98,6 +99,29 @@ const getSCVVTransform = version => {
       0.0,
       -1.0,
       0.0,
+      0.0,
+      0.0,
+      0.0,
+      1
+    ]
+  } else if (version == "0.2.0") {
+    console.log('fix 0.2.0')
+    return [
+      -1.0,
+      0.0,
+      0.0,
+      0.0,
+
+      0.0,
+      1.0,
+      0.0,
+      0.0,
+
+      0.0,
+      0.0,
+      -1.0,
+      0.0,
+
       0.0,
       0.0,
       0.0,
